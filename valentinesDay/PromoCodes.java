c18bed383b999a2cb15700eb58ce8c144f49e3ef
Vamsi Tallapudi <vamsi@coderefer.com>
2023-02-12 17:32:32 +0530

    bug fix changes

--- valentinesDay/PromoCodes.java
+++ valentinesDay/PromoCodes.java
@@ -1,6 +1,6 @@
 package valentinesDay;
 
-public class PromoCodes {
+public class PromoCodes implements ValentinesDay {
     public static void main(String[] args) {
         System.out.println("VALENTINE10");
     }
